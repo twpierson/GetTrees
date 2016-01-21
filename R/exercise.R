4d80d28319f287536a11e06b3e04da61bc1b586d
Todd Pierson <twpierson@twplaptop2.nomads.utk.edu>
2016-01-21 14:56:19 -0500

    rotl part is now working

--- R/exercise.R
+++ R/exercise.R
@@ -4,20 +4,21 @@
 # Sadly, most trees do not end up there, but those that do can be very useful.
 # Another issue is that until recenty TreeBASE prohibited branch length
 # information, though more recent trees do (often) have this info. 
+
 GetFromTreeBase_exercise <- function() {
 	# Get all trees for Formica (a genus of ants), searching by taxon, with branch
 	# lengths
 	# Replace the "__________" with appropriate arguments
 	# When you run this, it'll take some time. But less time than sequencing
 	# and building the tree yourself.
-	trees <- search_treebase(input="__________", by="__________", 
-	branch_lengths="__________")
+	trees <- search_treebase(input="Formica", by="taxon", 
+	branch_lengths=TRUE)
 	
 	# Pull out just the biggest tree (for convenience)
 	biggest.tree <- trees[[which.max(sapply(trees, Ntip))]]
 	
 	# Print out a description of the biggest tree
-	print("__________")
+	print(biggest.tree)
 	
 	# Let's look at the TreeBASE page for this study
 	browseURL(paste(
@@ -35,20 +36,21 @@ GetFromTreeBase_exercise <- function() {
 # TreeBASE, anyone, not just the author, can add trees to Open Tree's 
 # repository. Another important distinction is that Open Tree seeks to create
 # a single tree for all life by creating essentially a supreme super tree.
+
 GetTreeFromOpenTree_exercise <- function() {
 	# Now let's look for the gecko genus Anolis
 	# Figure out the id used for Anolis in Open Tree's database
 
 	# Replace the "__________" with appropriate arguments
 	
-	anolis.id <- tnrs_match_names("__________")$ott_id
+	anolis.id <- tnrs_match_names("Anolis")$ott_id
 	
 	# Now get Open Tree's current best estimate of the phylogeny for the group
 	# They call this the tree of life; we can get the subtree for just this group.
 	anolis.tree <- tol_subtree(ott_id=anolis.id)
 	
 	# Let's plot the tree:
-	plot.phylo("__________", type="fan", cex=0.2)
+	plot.phylo(anolis.tree, type="fan", cex=0.2)
 	
 	# It has a lot of polytomies, representing uncertainty. A maximally resolved
 	# tree (if rooted) will have one fewer internal nodes than terminal nodes:
@@ -66,18 +68,19 @@ GetTreeFromOpenTree_exercise <- function() {
 	"% resolved", sep=""))
 	
 	# Open Tree can also return the original studies with the source trees.
-	anolis.studies <- studies_find_studies(property="ot:focalCladeOTTTaxonName",
-	value="Anolis")
+	anolis.studies <- studies_find_studies(property="ot:focalCladeOTTTaxonName",value="Anolis")
 	anolis.studies.ids <- unlist(anolis.studies$matched_studies)
 	
 	# Let's get info on the first study
-	anolis.study1.metadata <- get_study_meta(anolis.studies[[1]][[1]]$`ot:studyId`)
+	#anolis.study1.metadata <- get_study_meta(anolis.studies[[1]][[1]]$`ot:studyId`)
+	anolis.study1.metadata <- get_study_meta(anolis.studies[[1]][[1]])
 	print(get_publication(anolis.study1.metadata))
 	
 	# And let's get the tree from this study
 	# Look in rotl documentation for the right function
 	# Hint: get_study_trees() is close, but you don't know the tree.id
-	anolis.study1.tree1 <- get_______FUNCTION_FROM_rotl____(anolis.studies.ids[[1]])
+	#anolis.study1.tree1 <- get_study_tree(anolis.studies.ids[[1]])
+	anolis.study1.tree1 <- get_study_tree(study_id=anolis.studies[[1]],tree_id=anolis.studies[[3]])
 	
 	# And plot it
 	plot.phylo(anolis.study1.tree1, type="fan", cex=0.2)
